be4d3b1141316508090770fd5278cfc83887eeb4
Aaron <aaronnader1996@gmail.com>
2023-05-19 13:17:20 -0600

    Fixes to Router

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/tailwind.css";
+import "./styles/main.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
